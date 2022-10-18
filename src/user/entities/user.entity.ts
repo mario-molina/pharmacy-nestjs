9b69d3928459a0888da681d775108e87e12eb5bc
mariomolina <super_mario2570@hotmail.com>
2022-10-18 12:46:01 -0600

    Added libraries to use with user authentication module

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,12 +1,14 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { ProductLike } from "../../product-likes/entities/product-like.entity";
+import { BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductLike } from '../../product-likes/entities/product-like.entity';
 
 @Entity('user')
 export class User {
+  readonly bcrypt = require('bcrypt');
+
   @PrimaryGeneratedColumn() // Creates an auto incremental id
   id: number;
 
-  @Column({ type: 'varchar', length: 15 })
+  @Column({ type: 'varchar', length: 15, unique: true })
   username: string;
 
   @Column({ type: 'varchar', length: 60 })
@@ -25,4 +27,9 @@ export class User {
   created_date: Date;
 
   @OneToMany(type => ProductLike, productLike => productLike.user) likes: ProductLike[];
+
+  //  the @BeforeInsert() hook gives the developer the opportunity to run any code before saving the Entity in the database
+  @BeforeInsert() async hashPassword() {
+    this.password = await this.bcrypt.hash(this.password, 10);
+  }
 }
