88b10f10708dae9c497cc69a29c0fd370f04a484
mario-molina <69019590+mario-molina@users.noreply.github.com>
2022-10-19 12:09:42 -0600

    Added encripted password to create user method

--- src/shared/util.ts
+++ src/shared/util.ts
@@ -2,3 +2,12 @@ export const comparePasswords = async (userPassword, currentPassword) => {
     let bcrypt = require('bcrypt');
     return await bcrypt.compare(currentPassword, userPassword);
 };
+
+export const encryptPasswords = async (userPassword, rand) => {
+    let bcrypt = require('bcrypt');
+    // generate salt to hash password
+    let salt = await bcrypt.genSalt(rand);
+    // now we set user password to hashed password
+    userPassword = await bcrypt.hash(userPassword, salt);
+    return userPassword
+};
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,7 +7,7 @@ import { Repository } from 'typeorm';
 import { UserDto } from './dto/user.dto';
 import { toUserDto } from 'src/shared/mapper';
 import { LoginUserDto } from './dto/login-user.dto';
-import { comparePasswords } from 'src/shared/util';
+import { comparePasswords, encryptPasswords } from 'src/shared/util';
 
 @Injectable()
 export class UserService {
@@ -16,10 +16,22 @@ export class UserService {
   constructor(@InjectRepository(User) userRepository: Repository<User>) {
     this.userRepository = userRepository;
   }
+
   async create(user: CreateUserDto) {
+
+    // check if the user exists in the db    
+    const userInDb = await this.userRepository.findOne({ 
+      where: { username : user.username } 
+    });
+    if (userInDb) {
+        throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);    
+    }
+
     user.created_date = new Date(user.created_date);
-    await this.userRepository.insert(user);
-    return user;
+    let encrypted_password = await encryptPasswords(user.password,10);
+    user.password = encrypted_password;
+    const newUser: User = await this.userRepository.insert(user);
+    return toUserDto(newUser);
   }
 
   async findAll() {
